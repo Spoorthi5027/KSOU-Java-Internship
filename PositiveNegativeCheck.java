42e4315be2034e438533d22ed7a434b03e05ccbc
Spoorthi5027 <spoorthiraghavendra9@gmail.com>
2026-02-10 10:43:17 +0530

    Add files via upload
    Added basic Java programs
    Initial commit - Java practice programs
    Added positive/negative, prime, reverse and print number programs

---
+++ PositiveNegativeCheck.java
@@ -0,0 +1,15 @@
+package loop;
+public class PositiveNegativeCheck {
+    public static void main(String[] args) {
+        int num = 123;
+
+        if (num > 0) {
+            System.out.println("Positive Number");
+        } else if (num < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
+
